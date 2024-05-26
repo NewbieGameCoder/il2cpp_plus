50d0a758282d7a667c76c7db5880b5ccbddb4c94
walon <walon@code-philosophy.com>
2024-05-26 11:27:18 +0800

    [refactor] GetReversePInvokeWrapper由MetadataModule移到InterpreterModule，相应调整代码

--- libil2cpp/vm/PlatformInvoke.cpp
+++ libil2cpp/vm/PlatformInvoke.cpp
@@ -19,6 +19,7 @@
 
 #include "hybridclr/metadata/MetadataUtil.h"
 #include "hybridclr/metadata/MetadataModule.h"
+#include "hybridclr/interpreter/InterpreterModule.h"
 
 #include <stdint.h>
 #include <algorithm>
@@ -423,7 +424,7 @@ namespace vm
         if (method && hybridclr::metadata::IsInterpreterImplement(method))
         {
             Il2CppCallConvention callConvention = GetDelegateCallConvention(d);
-            return reinterpret_cast<intptr_t>(hybridclr::metadata::MetadataModule::GetReversePInvokeWrapper(d->method->klass->image, method, callConvention));
+            return reinterpret_cast<intptr_t>(hybridclr::interpreter::InterpreterModule::GetReversePInvokeWrapper(d->method->klass->image, method, callConvention));
         }
 
         Il2CppMethodPointer reversePInvokeWrapper = MetadataCache::GetReversePInvokeWrapper(d->method->klass->image, d->method);
